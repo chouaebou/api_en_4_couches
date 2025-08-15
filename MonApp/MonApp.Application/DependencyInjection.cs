473c1db9e023364e571df13386bd35ce12f597ee
chouaebou <imounchigam@gmail.com>
2025-08-15 18:47:07 -0400

    Création du projet api

---
+++ MonApp/MonApp.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MonApp.Application.Interfaces;
+using MonApp.Application.Services;
+
+namespace MonApp.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            //services.AddScoped<IArticlesService, ArticlesService>();
+            services.AddScoped<IRecetteService, RecetteService>();
+            return services;
+        }
+            
+    }
+}
---
+++ MonApp/MonApp.Infrastructure/Repositories/RecetteRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MonApp.Domaine.Interfaces;
+using MonApp.Domaine.Models;
+using MonApp.Infrastructure.Data;
+
+namespace MonApp.Infrastructure.Repositories
+{
+    public class RecetteRepository: IRecetteRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RecetteRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Recette>> GetAllAsync() => await _context.Recettes.ToListAsync();
+
+        public async Task<Recette?> GetByIdAsync(int id) => await _context.Recettes.FindAsync(id);
+
+        public async Task<Recette> CreateAsync(Recette recette)
+        {
+            _context.Recettes.Add(recette);
+            await _context.SaveChangesAsync();
+            return recette;
+        }
+
+        public async Task<Recette> UpdateAsync(Recette recette)
+        {
+            _context.Recettes.Update(recette);
+            await _context.SaveChangesAsync();
+            return recette;
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            var recette = await _context.Recettes.FindAsync(id);
+            if (recette != null)
+            {
+                _context.Recettes.Remove(recette);
+                return await _context.SaveChangesAsync();
+            }
+            return -1;
+        }
+
+    }
+}
